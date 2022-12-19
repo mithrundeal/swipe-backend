d2aa1a213697180621f3b7789bda7d9024eb1a11
root-14 <ilkaygavaz2@gmail.com>
2022-12-19 19:55:39 +0300

    one commit to rule them all

---
+++ src/test/java/com/mithrundeal/swipebackend/SwipeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mithrundeal.swipebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwipeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
